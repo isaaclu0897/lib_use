01dab2944e950cbcebc1c35e306852b3ad8353cf
wei <t104306033@ntut.org.tw>
2018-04-04 22:48:04 +0800

    /opencv: threshold optimize

--- opencv/dealwith_image.py
+++ opencv/dealwith_image.py
@@ -27,3 +27,12 @@ imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 binary,contours,hierarchy = cv2.findContours(imgray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(im,contours,-1,(255, 0, 0), 2)
 cv2.imshow('im', im)
+
+#%% threshold optimize
+import cv2
+im = cv2.imread('raspberry.jpg')
+imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(imgray,235,255,0)
+binary,contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(im,contours,-1,(255, 0, 0), 2)
+cv2.imshow('im', im)
