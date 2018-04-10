6098ea86ddf6909e9106fdfe2cc49aa550a5da68
wei <t104306033@ntut.org.tw>
2018-04-10 15:50:52 +0800

    /matplotlib: create 3D figure

---
+++ matplot/matplot3d.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 22 14:36:56 2018
+
+@author: wei
+"""
+
+#import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D # import 3D module
+
+# create 3D figure
+fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d') 
+ax = Axes3D(fig)
+plt.show()
+
