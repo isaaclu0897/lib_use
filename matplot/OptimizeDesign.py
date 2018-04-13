26d4d40203edb9d14d7e8889a73480c4e6c0667a
wei <t104306033@ntut.org.tw>
2018-04-13 17:25:52 +0800

    /optimization: add reference to debug

--- matplot/OptimizeDesign.py
+++ matplot/OptimizeDesign.py
@@ -62,3 +62,36 @@ ax.plot_surface(X1, X2, F, rstride=1, cstride=1, cmap=plt.get_cmap('jet'))
 plt.show()
 
 
+# =============================================================================
+# reference
+# =============================================================================
+#import numpy as np
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
+#
+#point  = np.array([1, 2, 3])
+#normal = np.array([1, 1, 2])
+#
+#point2 = np.array([10, 50, 50])
+#
+## a plane is a*x+b*y+c*z+d=0
+## [a,b,c] is the normal. Thus, we have to calculate
+## d and we're set
+#d = -point.dot(normal)
+#
+## create x,y
+#xx, yy = np.meshgrid(range(10), range(10))
+#
+## calculate corresponding z
+#z = (-normal[0] * xx - normal[1] * yy - d) * 1. /normal[2]
+#
+## plot the surface
+#fig = plt.figure()
+#ax = Axes3D(fig)
+#ax.plot_surface(xx, yy, z, alpha=0.2)
+#
+#
+##and i would like to plot this point : 
+#ax.scatter(point2[0] , point2[1] , point2[2],  color='green')
+#
+#plt.show()
